f78c6558c57628f844d8b082b25d0bfedd8f15d4
Tommy Tran <tranttommy95@gmail.com>
2019-03-09 15:06:05 -0800

    added hashchange event listener

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import trackList from '../data/data.js';
 import loadTracks from './listing-component.js';
 import './search-component.js';
 
-loadTracks(trackList.tracks.items);
+const searchBar = document.getElementById('search-bar');
+const params = new URLSearchParams(window.location.hash.slice(1));
+searchBar.value = params.get('search');
+
+window.addEventListener('hashchange', () => {
+    loadTracks(trackList.tracks.items);
+});
