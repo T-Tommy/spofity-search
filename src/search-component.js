3d753015e42a662284866fcb81f14faf2b98ac2f
Tommy Tran <tranttommy95@gmail.com>
2019-03-09 12:43:49 -0800

    search submission updates hash

--- src/search-component.js
+++ src/search-component.js
@@ -1,7 +1,11 @@
+import { writeSearchToQuery } from './hash-query.js';
+
 const form = document.getElementById('form');
 const searchBar = document.getElementById('search-bar');
 form.addEventListener('submit', event => {
     event.preventDefault();
     const searchTerm = searchBar.value;
-    console.log(searchTerm);
+    const existingQuery = window.location.hash.slice(1);
+    const newQuery = writeSearchToQuery(existingQuery, searchTerm);
+    window.location.hash = newQuery;
 });
