64c595c660584f926e1dc30f6ae8444b2a0d0861
Tommy Tran <tranttommy95@gmail.com>
2019-03-09 15:23:09 -0800

    added readFromQuery to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import trackList from '../data/data.js';
 import loadTracks from './listing-component.js';
 import './search-component.js';
+import { readFromQuery } from './hash-query.js';
 
 const searchBar = document.getElementById('search-bar');
-const params = new URLSearchParams(window.location.hash.slice(1));
-searchBar.value = params.get('search');
+const existingQuery = window.location.hash.slice(1);
+const queryOptions = readFromQuery(existingQuery);
+searchBar.value = queryOptions.search;
 
 window.addEventListener('hashchange', () => {
     loadTracks(trackList.tracks.items);
