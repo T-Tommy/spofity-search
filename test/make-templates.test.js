c1e642db7e67c4a3328a0a3dab992ac667d0720d
Tommy Tran <tranttommy95@gmail.com>
2019-03-09 21:41:12 -0800

    forgot import

--- test/make-templates.test.js
+++ test/make-templates.test.js
@@ -1,4 +1,4 @@
-import { makeTrackRowTemplate, getArtists } from '../src/listing-component.js';
+import { makeTrackRowTemplate, makeTrackTableTemplate, getArtists } from '../src/listing-component.js';
 
 const test = QUnit.test;
 
