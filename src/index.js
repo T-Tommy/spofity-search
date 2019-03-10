5f71de1038d7e0a02df73e4d912f03dbf5424d51
Tommy Tran <tranttommy95@gmail.com>
2019-03-09 17:01:13 -0800

    refactor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import trackList from '../data/data.js';
 import loadTracks from './listing-component.js';
 import './search-component.js';
 import displayParams from './display-params.js';
+import { readFromQuery } from './hash-query.js';
+import makeApiSearchUrl from './make-api-search-url.js';
+
 
 if(window.location.hash.slice(1)) {
     displayParams();
 }
 
-window.addEventListener('hashchange', () => {
+window.addEventListener('hashchange', loadPage);
+
+function loadPage() {
+    const exisitingQuery = window.location.hash.slice(1);
+    const queryOptions = readFromQuery(exisitingQuery);
+    const url = makeApiSearchUrl(queryOptions);
+    console.log(url);
     loadTracks(trackList.tracks.items);
-});
+}
