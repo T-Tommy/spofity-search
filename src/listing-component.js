d93125709041630055cfb6cd47ef17edded955bf
Tommy Tran <tranttommy95@gmail.com>
2019-03-13 16:25:53 -0700

    can add favorites

--- src/listing-component.js
+++ src/listing-component.js
@@ -1,9 +1,27 @@
+import { auth, favoritesByUserRef } from './firebase.js';
 const tbody = document.getElementById('track-tbody');
 
 export default function loadTracks(trackList) {
     clearList();
     trackList.forEach(track => {
         const dom = makeTrackRowTemplate(track);
+        const tr = dom.querySelector('tr');
+
+        const userId = auth.currentUser.uid;
+        const userFavoritesRef = favoritesByUserRef.child(userId);
+        const userFavoriteTrackRef = userFavoritesRef.child(track.name);
+
+        tr.addEventListener('click', () => {
+            if(tr.classList.contains('favorite')) {
+                tr.classList.remove('favorite');
+            } else {
+                tr.classList.add('favorite');
+                userFavoriteTrackRef.set({
+                    name: track.name,
+                    artist: getArtists(track.artists),
+                });
+            }
+        });
         tbody.appendChild(dom);
     });
 }
