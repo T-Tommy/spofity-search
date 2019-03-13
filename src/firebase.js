4df23b101cc8b752daf1c72c7139fd6f34e6b3af
Tommy Tran <tranttommy95@gmail.com>
2019-03-13 13:44:45 -0700

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: 'AIzaSyCF7eEs1BuIA0aq-9b_r7VpyCZ5RdqEUho',
+    authDomain: 'spofity-search.firebaseapp.com',
+    databaseURL: 'https://spofity-search.firebaseio.com',
+    projectId: 'spofity-search',
+};
+
+firebase.initializeApp(config);
+
+
